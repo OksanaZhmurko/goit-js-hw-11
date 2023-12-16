b0d2e8f0862174e1c2f0b3a13ce92b8b43939c5c
OksanaZhmurko <oksanazhmurko@gmail.com>
2023-12-17 00:56:58 +0200

    update

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,7 +1,49 @@
 
 import { getPhoto } from "./pixabay-api";
+import Notiflix from "notiflix";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-getPhoto("cat", 1)
-.then(res => {
-    console.log(res);
-})
+const formEl = document.getElementById("search-form");
+const gallery = document.querySelector(".gallery");
+
+
+formEl.addEventListener("submit", onSubmit);
+
+let page = 1;
+
+async function onSubmit(event) {
+    event.preventDefault();
+    const searchQuery = event.target.elements.searchQuery.value;
+   
+    try {
+        const {data: {hits, total}} = await getPhoto(searchQuery, page);
+        if(hits.length === 0){
+            Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+        }
+        gallery.innerHTML = createMarkup(hits);    
+    } catch (error) {
+        
+    }
+}
+
+function createMarkup(arr) {
+    return arr.map(item => `  
+    <div class="photo-card">
+     <img src="${item.largeImageURL}" alt="${item.tags}" loading="lazy" />
+     <div class="info">
+       <p class="info-item">
+         <b>Likes:</b> ${item.likes}
+       </p>
+       <p class="info-item">
+         <b>Views:</b>${item.views}
+       </p>
+       <p class="info-item">
+         <b>Comments:</b>${item.comments}
+       </p>
+       <p class="info-item">
+         <b>Downloads:</b>${item.downloads}
+       </p>
+     </div>
+   </div>`).join ("");
+
+}
